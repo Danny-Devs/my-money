c70215a1dc2d831fac46dc0c4364d1314423c141
Daniel Ahn <et3rnal.d@gmail.com>
2021-12-22 21:33:29 -0800

    complete to ch 125. add cleanup functions to useLogout and useSignup custom hooks

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,12 @@
-import { useState } from "react"
-import { projectAuth } from "../firebase/config"
+import { useEffect, useState } from 'react'
+import { projectAuth } from '../firebase/config'
+import { useAuthContext } from './useAuthContext'
 
 export const useSignup = () => {
+  const [isCanceled, setIsCanceled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
 
   const signup = async (email, password, displayName) => {
     setError(null)
@@ -11,8 +14,10 @@ export const useSignup = () => {
 
     try {
       // signup user
-      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
-      console.log(res.user);
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      )
 
       if (!res) {
         throw new Error('Could not complete signup')
@@ -21,16 +26,26 @@ export const useSignup = () => {
       // add display name to user
       await res.user.updateProfile({ displayName })
 
-      setIsPending(false)
-      setError(null)
-      
+      // dispatch login action
+      dispatch({ type: 'LOGIN', payload: res.user })
+
+      // update state. the if check works with the cleanup function
+      if (!isCanceled) {
+        setIsPending(false)
+        setError(null)
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message)
-      setIsPending(false)
+      if (!isCanceled) {
+        console.log(err.message)
+        setError(err.message)
+        setIsPending(false)
+      }
     }
   }
 
+  useEffect(() => {
+    return () => setIsCanceled(true)
+  }, [])
+
   return { error, isPending, signup }
 }
-
